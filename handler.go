42c84837a58a0afffba06bec6a1e7688334d0315
Shiv10 <shivanshsharma2012@gmail.com>
2024-09-16 00:59:49 +0530

    feat: add hget and hset

--- handler.go
+++ handler.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"sync"
 )
 
 var SETs = map[string]string{}
 var SETsMu = sync.RWMutex{}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -50,8 +54,51 @@ func get(args []Value) Value {
 	return Value{typ: "bulk", bulk: val}
 }
 
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: fmt.Sprintf("ERR wrong number of arguments. Exepcted 3 got %d", len(args))}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	val := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = val
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: fmt.Sprintf("ERR wrong number of arguments. Exepcted 2 got %d", len(args))}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.Lock()
+	m, ok := HSETs[hash]
+	if !ok {
+		return Value{typ: "error", str: "ERR, hash does not exist"}
+	}
+
+	val, ok := m[key]
+	if !ok {
+		return Value{typ: "error", str: "ERR, key does not exist inside hash"}
+	}
+
+	return Value{typ: "bulk", bulk: val}
+}
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"SET": set,
 	"GET": get,
+	"HSET": hset,
+	"HGET": hget,
 }
