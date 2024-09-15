ed85a818008c990abc7f8a53ee0301366d470373
Shiv10 <shivanshsharma2012@gmail.com>
2024-09-16 00:34:17 +0530

    feat: add get and set commands

--- handler.go
+++ handler.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	// "fmt"
+	"sync"
+)
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+
 func ping(args []Value) Value {
 	if len(args) == 0 {
 		return Value{typ: "string", str: "PONG"}
@@ -8,9 +16,42 @@ func ping(args []Value) Value {
 	return Value{typ: "string", str: args[0].bulk}
 }
 
-var Handlers = map[string]func([]Value) Value {
-	"PING": ping,
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR invalid number of arguments for SET command"}
+	}
+
+	key := args[0].bulk
+	val := args[1].bulk
+
+	SETsMu.Lock()
+	SETs[key] = val
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
 }
 
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR invalid number of arguments for GET command"}
+	}
 
+	key := args[0].bulk
 
+	SETsMu.Lock()
+	// fmt.Println(SETs)
+	val, ok := SETs[key]
+	SETsMu.Unlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: val}
+}
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
+	"SET": set,
+	"GET": get,
+}
